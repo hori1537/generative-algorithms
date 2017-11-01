e3768786596fd763f34e32965edc4aa26489f2bf
hori1537 <yuki.horie@cgco.co.jp>
2017-11-01 20:20:46 +0900

    Add files via upload

--- generative-algorithm.py
+++ generative-algorithm.py
@@ -11,12 +11,14 @@ import pandas as pd
 
 import tkinter
 from tkinter import filedialog
+from tkinter import messagebox
 
 import xlrd
 import os
 import os.path
 import sys
 
+from scipy.stats import shapiro
 
 import keras
 # from keras.utils import plot_model
@@ -115,30 +117,29 @@ param_priority      = np.array(sht1_col[3][1:6])
 param_over          = np.array(sht1_col[4][1:6])
 param_under         = np.array(sht1_col[5][1:6])
 param_equal         = np.array(sht1_col[6][1:6])
-param_tgt           = np.array(sht1_col[7][1:6])
+param_target           = np.array(sht1_col[7][1:6])
 param_unit          = np.array(sht1_col[8][1:6])
 param_avg           = np.array(sht1_col[9][1:6])
 param_std           = np.array(sht1_col[10][1:6])
 
 param_num           = len(param_formalname)
-print('param_num is ',param_num)
 
 ### import target component of glass from xlsx
 
-compo_name          = np.array(sht2_col[1][1:63])
+component_name          = np.array(sht2_col[1][1:63])
 
 
-compo_must          = np.array(sht2_col[2][1:63])
-compo_can           = np.array(sht2_col[3][1:63])
-compo_better        = np.array(sht2_col[4][1:63])
-compo_no            = np.array(sht2_col[5][1:63])
-compo_over          = np.array(sht2_col[6][1:63])
-compo_under         = np.array(sht2_col[7][1:63])
-compo_equal         = np.array(sht2_col[8][1:63])
-compo_tgt           = np.array(sht2_col[9][1:63])
+component_must          = np.array(sht2_col[2][1:63])
+component_can           = np.array(sht2_col[3][1:63])
+component_better        = np.array(sht2_col[4][1:63])
+component_no            = np.array(sht2_col[5][1:63])
+component_over          = np.array(sht2_col[6][1:63])
+component_under         = np.array(sht2_col[7][1:63])
+component_equal         = np.array(sht2_col[8][1:63])
+component_target           = np.array(sht2_col[9][1:63])
 
-max_component_size  = len(compo_name)
-print('max_component_size is ', max_component_size)
+max_component_size  = len(component_name)
+
 
 ### setting values of generative algorithm from xlsx
 
@@ -155,29 +156,30 @@ mut_indpb           = sht3_col[7][3]
 
 is_grid_search      = sht3_col[8][3]
 
-print('num_gene_',num_gene)
-print('num_pop_',num_population)
-print('num_generation_',num_generation)
-print('num_gene_',num_gene)
-
-
 ### fit_weights of 5 parameters from xlsx
-fit_weights = np.array(param_priority) * (-1)
+### convert positive to negative
+
+fit_weights = np.array(param_priority)
+fit_weights = fit_weights * (-1)
 
 ### add fit_weight of component number
 fit_weights = np.append(fit_weights , 0)
+
+### add -0.0001 to the each fit_weight
+### when fit_weight value is 0 cause error
+fit_weights = fit_weights - 0.001
 fit_weights = list(fit_weights)
 
 
-compo_must_and_can = np.array(compo_must) + np.array(compo_can)
-print('compo_must_and_can is ' ,compo_must_and_can)
+component_must_and_can = np.array(component_must) + np.array(component_can)
+print('component_must_and_can is ' ,component_must_and_can)
 
-#compo_list          = compo_must_and_can
-compo_list          = np.nonzero(compo_must_and_can)
-compo_list          = np.array(compo_list)
-compo_list          = compo_list.reshape(-1,)
+#component_list          = component_must_and_can
+component_list          = np.nonzero(component_must_and_can)
+component_list          = np.array(component_list)
+component_list          = component_list.reshape(-1,)
 
-compo_tgt           = 5     #-1
+component_target           = 5     #-1
 
 '''
 ### default values of generative algorithm
@@ -196,24 +198,24 @@ is_grid_search     = 0
 ### defalut values of setting
 #param_num           = 5
 #max_component_size  = 62
-#compo_list          = [0,2,5,7,8,9,10,11]
+#component_list          = [0,2,5,7,8,9,10,11]
 
 
 
 '''
-### defalut param_tgt
+### defalut param_target
 
 param_name = ['ABBE','DENS','FRAC','POIS','YOUN']
 
-param_tgt = np.empty(param_num)
+param_target = np.empty(param_num)
 param_avg = np.empty(param_num)
 param_std = np.empty(param_num)
 
-param_tgt[0] = 15
-param_tgt[1] = 3.227
-param_tgt[2] = 0.47
-param_tgt[3] = 0.28
-param_tgt[4] = 90.0
+param_target[0] = 15
+param_target[1] = 3.227
+param_target[2] = 0.47
+param_target[3] = 0.28
+param_target[4] = 90.0
 
 param_avg[0] = 49.30
 param_avg[1] = 2.658
@@ -227,7 +229,6 @@ param_std[2] = 0.63381
 param_std[3] = 0.03073
 param_std[4] = 48.2235
 '''
-
 # 7241
 # GP09-308146
 # SiO2_46.42
@@ -262,16 +263,19 @@ YOUN_target= 90.0   #0
 
 model = [0] * param_num
 for i in range(5):
-    modelfile = ('C:\deeplearning/model/model_' + param_name[i] + '.h5')
+    modelfile = ('C:\deeplearning/model/model_' + param_name[i] + '_n.h5')
     if os.path.exists(modelfile):
         model[i] = keras.models.load_model(modelfile)
     else:
         print(modelfile, ' does not exist. Then program exit')
+        messagebox.showerror(title='error', message=(modelfile, ' does not exist. Then program exit'))
         sys.exit()
 
 
 ### creator
-creator.create("FitnessMulti", base.Fitness, weights=fit_weights)
+creator.create("FitnessMulti", base.Fitness, weights = fit_weights)
+#creator.create("FitnessMulti", base.Fitness, weights = (-1.0,0.0001))
+
 creator.create("Individual", np.ndarray, fitness=creator.FitnessMulti)
 toolbox = base.Toolbox()
 
@@ -293,32 +297,33 @@ def analyze_Gene(individual):
 
     return [component, component_size]
 
-### evaluate(predict) properties from glass component using deeplearning model by keras
-def evalProperties(component):
-    # evaluate the properties from component
+### evaluate(predict) nomalized properties from glass component using deeplearning model by keras
+def eval_nomalizedproperties(component):
 
-    param_predict = np.empty(param_num)
+    param_predict_nomalized = np.empty(param_num)
 
     for i in range(param_num):
-        param_predict[i] = model[i].predict(component, batch_size=1, verbose=0)
+        param_predict_nomalized[i] = model[i].predict(component, batch_size=1, verbose=0)
 
-    param_predict = np.array(param_predict)
+    param_predict_nomalized = np.array(param_predict_nomalized)
 
-    return param_predict
+    return param_predict_nomalized
 
 ### nomalize the properties
 def nomalization(param):
     # parameter nomalization
 
-    param_nom = np.empty(param_num)
-    param_nom = (param - param_avg) / param_std
+    param_nomalized = (param - param_avg) / param_std
 
-    '''
-    for i in range(param_num):
-        param_nom[i] = (param[i]-param_avg[i])/param_std[i]
-    '''
+    return param_nomalized
 
-    return param_nom
+
+### denomalize the nomalized properties
+def de_nomalization(param_nomalized):
+    param = param_nomalized * param_std + param_avg
+    
+    return param
+
 
 ### evaluate the squared error:target value and predict value
 def evalERROR(individual):
@@ -326,27 +331,33 @@ def evalERROR(individual):
 
     component, component_size = analyze_Gene(individual)
 
-    param_predict = evalProperties(component)
-    param_predict = np.array(param_predict)
+    param_predict_nomalized = eval_nomalizedproperties(component)
+    param_predict_nomalized = np.array(param_predict_nomalized)
+    param_predict           = de_nomalization(param_predict_nomalized)
 
-    param_predict_nom = nomalization(param_predict)
 
-    param_tgt_nom = nomalization(param_tgt)
+    param_target_nomalized = nomalization(param_target)
 
-    param_square_error = (param_predict_nom - param_tgt_nom) ** 2
+    param_square_error = (param_predict_nomalized - param_target_nomalized) ** 2
 
-    component_size_square_error = (component_size - compo_tgt) ** 2
+    component_size_square_error = (component_size - component_target) ** 2
 
-    # for i in range(param_num):
-    # print(param_name[i], '_predict is ' , param_predict[i])
-    # print(param_name[i], '_square_error is ' , param_square_error[i])
+    #for i in range(param_num):
+        #print(param_name[i], '_predict_nomalized is ' , param_predict_nomalized[i])
+        #print(param_name[i], '_predict is ', param_predict[i])
+        #print(param_name[i], '_square_error is ' , param_square_error[i])
 
-    # print('component is', component)
-    # print('component_size is ', component_size)
-    # print('component_size_square_error ', component_size_square_error)
+    #print('component is', component)
+    #print('component_size is ', component_size)
+    #print('component_size_square_error ', component_size_square_error)
 
-    return param_square_error[0], param_square_error[1], param_square_error[2], param_square_error[3], \
-           param_square_error[4], component_size_square_error
+    return param_square_error[0], param_square_error[1], param_square_error[2], param_square_error[3], param_square_error[4], component_size_square_error
+
+
+def evalshapiro(individual):
+    return shapiro(individual)[1],shapiro(individual)[1]
+
+
 
 ### mutatie each gene: convert to integer
 def mutUniformINT(individual, min_ind, max_ind, indpb):
@@ -361,7 +372,7 @@ def mutUniformINT(individual, min_ind, max_ind, indpb):
 
 ### mutatie each gene: convert to integer in INT_list
 def mutUniformINTfromlist(individual, INT_list, indpb):
-    # mutation:gene choosed from compo_list
+    # mutation:gene choosed from component_list
 
     size = len(individual)
     # print('size is ' , size)
@@ -372,42 +383,48 @@ def mutUniformINTfromlist(individual, INT_list, indpb):
 
 ### toolbox:attr_bool,individial,population
 
-toolbox.register("attr_bool", np.random.choice, compo_list)
+toolbox.register("attr_bool", np.random.choice, component_list)
 toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=num_gene)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
 
 ### toolbox:evaluate,mate,mutate,select
 toolbox.register("evaluate", evalERROR)
 toolbox.register("mate", tools.cxUniform, indpb=cx_indpb)
-toolbox.register("mutate", mutUniformINTfromlist, INT_list=compo_list, indpb=mut_indpb)
-#toolbox.register("mate", tools.cxUniform, indpb=0.3)
-#toolbox.register("mutate", mutUniformINTfromlist, INT_list=compo_list, indpb=0.3)
-# toolbox.register("mutate", mutUniformINT, min_ind=min_ind, max_ind=max_ind, indpb=0.1)
+toolbox.register("mutate", mutUniformINTfromlist, INT_list=component_list, indpb=mut_indpb)
 
-toolbox.register("select", tools.selNSGA2)
+#toolbox.register("mutate", mutUniformINTfromlist, INT_list=component_list, indpb=0.3)
 
-# toolbox.register("select", tools.selRoulette, k=num_population)
-# toolbox.register("select", tools.selTournament, tournsize=3)
+
+#toolbox.register("select", tools.selNSGA2)
+#toolbox.register("select", tools.selRoulette, k=num_population)
+toolbox.register("select", tools.selTournament, tournsize=3)
 
 def generative_algorithm():
     random.seed(64)
-    print(num_population)
+
     pop = toolbox.population(n=num_population)
     hof = tools.HallOfFame(num_population, similar=np.array_equal)
 
     # hof = tools.ParetoFront(similar=np.array_equal)
     # hof = tools.HallOfFame(1)
 
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", np.mean)
-    stats.register("std", np.std)
-    stats.register("min", np.min)
-    stats.register("max", np.max)
+    stats = tools.Statistics(key = lambda ind: ind.fitness.values)
+    stats.register("avg",  np.mean)
+    stats.register("std",  np.std)
+    stats.register("min",  np.min)
+    stats.register("max",  np.max)
 
     # algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, num_generation=1, stats=stats)
     # algorithms.eaMuPlusLambda(pop, toolbox, cxpb=0.5,lambda = 5 , mutpb=0.2, num_generation=1, stats=stats, halloffame=hof)
 
-    print('num_generation_', num_generation)
+    print('num_gene is ', num_gene)
+    print('num_pop is ', num_population)
+    print('num_generation is ', num_generation)
+    print('param_num is ', param_num)
+    print('max_component_size is ', max_component_size)
+    print('fit_weights is ', fit_weights)
+
     algorithms.eaSimple(pop, toolbox, cxpb=0.7, mutpb=0.05, ngen=num_generation, stats=stats, halloffame=hof)
 
     better_ind = tools.selBest(pop, 1)[0]
@@ -416,7 +433,7 @@ def generative_algorithm():
     # print('better comp is ', better_comp)
     # print('better component_size is ', better_component_size)
 
-    param_better = evalProperties(better_comp)
+    param_better = eval_nomalizedproperties(better_comp)
 
     # print('better ind is ', better_ind)
     # for i in range(param_num):
@@ -427,7 +444,7 @@ def generative_algorithm():
 
 
 def grid_search():
-    individual = np.random.choice(compo_list)
+    individual = np.random.choice(component_list)
 
 
 
@@ -442,19 +459,18 @@ if __name__ == "__main__":
         pop, stats, hof = generative_algorithm()
 
         #pop, hof = main()
-        hof0_comp, hof0_component_size = analyze_Gene(hof[0])
+        component_hof0, component_size_hof0 = analyze_Gene(hof[0])
+        print(component_hof0)
 
-        #hof_comp, hof_component_size = analyze_Gene(hof)
+        param_hof0_nomalized = eval_nomalizedproperties(component_hof0)
+        param_hof0 =de_nomalization(param_hof0_nomalized)
 
-        param_hof0 = evalProperties(hof0_comp)
-        print(hof0_comp)
-
-        df1     = pd.DataFrame(compo_name)
-        df2     = pd.DataFrame(hof0_comp)
+        df1     = pd.DataFrame(component_name)
+        df2     = pd.DataFrame(component_hof0)
         df1_2   = pd.concat([df1,df2.T],axis=1)
 
         df3 = pd.DataFrame(param_name)
-        df4 = pd.DataFrame(param_hof0)
+        df4 = pd.DataFrame(param_hof0_nomalized)
         df3_4 = pd.concat([df3, df4],axis=1)
 
         print(df1_2)
@@ -469,11 +485,15 @@ if __name__ == "__main__":
 
 
         print(str(currentdirectory) + '/hof_0.csv')
+        print('fit_weights is ', fit_weights)
 
         for i in range(param_num):
+            print(param_name[i], '_target is ', param_target[i])
             print(param_name[i], '_hall of fame is ', param_hof0[i])
 
         print('finish')
+        #messagebox.showinfo(title = 'finish', message = 'finish')
+
 
     elif is_grid_search == 1:
         pop, stats, hof = grid_search()
